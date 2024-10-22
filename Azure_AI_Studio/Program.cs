b60226d01f1c3589ba58714425871596f1733a2d
Luccas Carneiro <luccas.carneiro@smartconsulting.com.br>
2024-10-21 23:43:57 -0300

    First Commit

---
+++ Azure_AI_Studio/Program.cs
@@ -0,0 +1,65 @@
+﻿using Azure.AI.OpenAI;
+using Azure;
+using static System.Environment;
+using Azure.AI.Language.Conversations;
+using System.Net;
+
+namespace ChatGPT
+{
+    public class Program
+    {
+        private static string endpoint = "";
+        private static string key = "";
+
+        //Altere a task para que seja uma conversa natural, perguntas e respostas ate o usuario decidir encerrar
+        public static async Task Main(string[] args)
+        {
+            OpenAIClient client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+            Console.WriteLine("Bem-vindo ao Chatbot! Digite 'Encerrar Chat' para sair.");
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\nVocê: ");
+                Console.ResetColor();
+
+                string userInput = Console.ReadLine();
+
+                if (userInput.Equals("Encerrar Chat", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Chat encerrado. Até mais!");
+                    break;
+                }
+
+                var chatCompletionsOptions = new ChatCompletionsOptions()
+                {
+                    DeploymentName = "gpt-35-turbo-16k",
+                    Messages =
+                    {
+                        new ChatRequestSystemMessage("Você é um tradutor de portugues para ingles, todo o texto que voce recebe voce traduz e retorna para o usuario, caso tenham erros de ortografia ou gramatica corrija. Voce não da detalhes, apenas traduz todo o texto que recebe"),
+                        new ChatRequestUserMessage(userInput),
+                    },
+                    MaxTokens = 100
+                };
+
+                Console.WriteLine();
+
+                await foreach (StreamingChatCompletionsUpdate chatUpdate in client.GetChatCompletionsStreaming(chatCompletionsOptions))
+                {
+                    if (chatUpdate.Role.HasValue)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"{chatUpdate.Role.Value.ToString().ToUpperInvariant()}: ");
+                        Console.ResetColor();
+                    }
+                    if (!string.IsNullOrEmpty(chatUpdate.ContentUpdate))
+                    {
+                        Console.Write(chatUpdate.ContentUpdate);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DALLE/Program.cs
@@ -0,0 +1,45 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using System.Diagnostics;
+using static System.Environment;
+
+namespace DALLE
+{
+    public class Program
+    {
+        // add an async Main method:
+        public static async Task Main(string[] args)
+        {
+
+            string endpoint = "";
+            string key = "";
+
+            OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+
+
+            Response<ImageGenerations> imageGenerations = await client.GetImageGenerationsAsync(
+                new ImageGenerationOptions()
+                {
+                    DeploymentName = "Dalle3",
+                    Prompt = "Em uma praia cristalina brasileira, esta um casal o homem tem cabelo loiro e pele clara ela de cabelos castanhos e pele bronzeada, abraçados, eles estao contemplando o por do sol enquanto apreciam as ondas do mar, ao redor do mar a natureza tropical é evidente, a horientacao da imagem mostra o casal de costas com o mar ao fundo",
+                    Size = ImageSize.Size1024x1024,
+                });
+
+            // Image Generations responses provide URLs you can use to retrieve requested images
+            Uri imageUri = imageGenerations.Value.Data[0].Url;
+
+            // Print the image URI to console:
+            Console.WriteLine(imageUri);
+
+            // Open the image URL in the default web browser
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = imageUri.AbsoluteUri,
+                UseShellExecute = true
+            });
+
+            Console.ReadKey();
+
+        }
+    }
+}
